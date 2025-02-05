f4f87e04fb00eeda1d167fc2fa9c3b9eead5fb98
kakharovshohjahon7 <01kakharov@gmail.com>
2025-02-06 01:57:28 +0500

    pr

--- ecommerce-master/ecommerce/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from ecommerce import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail/<int:pk>/', views.product_detail, name='product_detail')
-]
